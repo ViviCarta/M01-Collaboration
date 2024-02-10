5d03d327fa8419e948e51c2b6795d28e483fb080
vivicarta <msaron@ivytech.edu>
2024-02-10 13:16:17 -0500

    Added M04 Prg Ass

---
+++ zoo.py
@@ -0,0 +1,9 @@
+# M04 Programming Assignment - Modules & Databases
+
+# 11.1
+
+# Make a file called zoo.py, defining a function to 
+# print a specific string
+
+def hours():
+    print('Open 9-5 daily')
